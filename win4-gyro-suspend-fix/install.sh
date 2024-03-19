0185663012df20d472b5acacb4cc370836f9218b
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-03-19 00:19:12 -0400

    update install script

--- win4-gyro-suspend-fix/install.sh
+++ win4-gyro-suspend-fix/install.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/bash
 
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root. use sudo" >&2
+if [ "$(id -u)" -eq 0 ]; then
+    echo "This script must not be run as root. don't use sudo" >&2
     exit 1
 fi
+
+echo "starting install of gyro fix"
+
+# remove if somehow already present
+sudo rm -rf /tmp/gpd-win-tricks
 
 cd /tmp
 
@@ -11,28 +16,28 @@ git clone https://github.com/aarron-lee/gpd-win-tricks.git
 
 cd gpd-win-tricks/win4-gyro-suspend-fix
 
-cat <<EOF > "/etc/device-quirks/systemd-suspend-mods.conf"
+sudo cat <<EOF > "/etc/device-quirks/systemd-suspend-mods.conf"
 bmi260_i2c
 bmi260_core
 EOF
 
 
-cp ./suspend-mods.sh /usr/local/bin/suspend-mods
-cp ./resume-mods.sh /usr/local/bin/resume-mods
+sudo cp ./suspend-mods.sh /usr/local/bin/suspend-mods
+sudo cp ./resume-mods.sh /usr/local/bin/resume-mods
 
-chmod +x /usr/local/bin/suspend-mods
-chmod +x /usr/local/bin/resume-mods
+sudo chmod +x /usr/local/bin/suspend-mods
+sudo chmod +x /usr/local/bin/resume-mods
 
-chcon -u system_u -r object_r --type=bin_t /usr/local/bin/suspend-mods
-chcon -u system_u -r object_r --type=bin_t /usr/local/bin/resume-mods
+sudo chcon -u system_u -r object_r --type=bin_t /usr/local/bin/suspend-mods
+sudo chcon -u system_u -r object_r --type=bin_t /usr/local/bin/resume-mods
 
-cp gyro-resume-fix.service /etc/systemd/system
-cp gyro-suspend-fix.service /etc/systemd/system
+sudo cp gyro-resume-fix.service /etc/systemd/system
+sudo cp gyro-suspend-fix.service /etc/systemd/system
 
-systemctl daemon-reload
-systemctl enable --now gyro-resume-fix.service
-systemctl enable --now gyro-suspend-fix.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now gyro-resume-fix.service
+sudo systemctl enable --now gyro-suspend-fix.service
 
 echo "installation complete!"
 
-rm -rf /tmp/gpd-win-tricks
+sudo rm -rf /tmp/gpd-win-tricks
