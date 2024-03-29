09d362fd9fe083df4321df521884307b48654a77
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-03-29 18:44:23 -0400

    wm2 comments

--- win4-gyro-suspend-fix/suspend-mods.sh
+++ win4-gyro-suspend-fix/suspend-mods.sh
@@ -4,3 +4,7 @@
 
 modprobe -r bmi260_i2c
 modprobe -r bmi260_core
+
+# WM2?
+#echo disabled | tee /sys/bus/i2c/devices/i2c-GXTP7385:00/power/wakeup
+#echo disabled | tee /sys/bus/i2c/devices/i2c-PNP0C50:00/power/wakeup
